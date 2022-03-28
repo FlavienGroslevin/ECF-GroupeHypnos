6631a8c89455455c277c7960103e61a48cc3e9fb
FlavienGroslevin <flaviengroslevin@gmail.com>
2022-03-28 20:34:50 +0200

    Mise en place du filtre sur la page réservation pour afficher les suites une fois l'hotel selectionné en ajax

--- src/Controller/ReservationController.php
+++ src/Controller/ReservationController.php
@@ -2,17 +2,22 @@
 
 namespace App\Controller;
 
+use App\Entity\HotelRooms;
+use App\Entity\ReservationRooms;
 use App\Form\ReservationType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ReservationController extends AbstractController
 {
     #[Route('/reservation', name: 'app_reservation')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
-        $form = $this->createForm(ReservationType::class);
+        $reservationRooms = new ReservationRooms();
+        $form = $this->createForm(ReservationType::class, $reservationRooms);
+        $form->handleRequest($request);
 
         return $this->render('reservation/index.html.twig', [
             'controller_name' => 'ReservationController',
--- src/Form/ReservationType.php
+++ src/Form/ReservationType.php
@@ -2,11 +2,18 @@
 
 namespace App\Form;
 
+use App\Entity\HotelRooms;
+use App\Entity\Hotels;
 use App\Entity\ReservationRooms;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
-use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Form;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
+use Symfony\Component\Form\FormInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class ReservationType extends AbstractType
@@ -18,17 +25,47 @@ class ReservationType extends AbstractType
                 'label' => "Date d'arrivée",
                 'widget' => 'single_text',
                 'html5' => false,
+                'empty_data' => null,
                 'attr' => ["class" => 'start'],
             ])
             ->add('end_date', DateType::class, [
                 'label' => "Date de départ",
                 'widget' => 'single_text',
                 'html5' => false,
+                'empty_data' => null,
                 'attr' => ["class" => 'end'],
             ])
-            ->add('hotels')
-            ->add('hotelRooms')
+            ->add('hotels', EntityType::class, [
+                'label' => "Choix de l'hôtel",
+                'class' => Hotels::class,
+                'placeholder' => 'Choisir un hôtel'
+            ])
         ;
+        $formModifier = function (FormInterface $form, Hotels $hotels = null){
+            $hotelRooms = null === $hotels ? [] : $hotels->getHotelRooms();
+
+            $form->add('hotelRooms', EntityType::class, [
+                'class' => HotelRooms::class,
+                'choices' => $hotelRooms,
+                'label' => 'Choix de la suite',
+            ]);
+        };
+
+        $builder->addEventListener(
+            FormEvents::PRE_SET_DATA,
+            function (FormEvent $event) use ($formModifier){
+             $data = $event->getData();
+             $formModifier($event->getForm(), $data->getHotelRooms());
+            }
+        );
+
+        $builder->get('hotels')->addEventListener(
+            FormEvents::POST_SUBMIT,
+            function (FormEvent $event) use ($formModifier) {
+                $hotel = $event->getForm()->getData();
+                $formModifier($event->getForm()->getParent(), $hotel);
+            }
+        );
     }
 
     public function configureOptions(OptionsResolver $resolver): void
